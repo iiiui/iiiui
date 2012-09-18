cb9b707c24bdfa7f3956d283ebf97af4b16cdeb5
vissul <gvissul@gmail.com>
2012-09-18 22:54:49 +0800

    update share save method

--- app/assets/javascripts/models/share.js
+++ app/assets/javascripts/models/share.js
@@ -1,10 +1,16 @@
 var Share = Backbone.Model.extend({
-  url: '/shares',
-    share_user_id: '',
-    share_user_shares_id:''
+    urlRoot: '/shares',
+    defaults: {
+      id: null,
+      user_id: '',
+      user_shares_id:''
+    },
+    toJSON: function() {
+        return { share: _.clone( this.attributes ) }
+    }
 })
 
 var Shares = Backbone.Collection.extend({
-  model: Shares
+  model: Shares,
 })
 
--- app/assets/javascripts/views/shareview.js
+++ app/assets/javascripts/views/shareview.js
@@ -1,20 +1,16 @@
 var NewSharesView = Backbone.View.extend({
-  el: $('#new_share'),
     events: {
       'click #addShare': 'addShare'
     },
 
     initialize: function() {
-      // console.log("init Share");
-      // this.collection.on('add', this.clearInput, this);
-      share = new Share();
     },
 
     addShare: function(e) {
-      // console.log("addShare");
       e.preventDefault();
-      share.share_user_id =  $("#share_user_id").val();
-      share.share_user_shares_id = $("#share_user_shares_id").val();
+      share = new Share();
+      share.set('user_id',$("#share_user_id").val());
+      share.set('user_shares_id',$("#share_user_shares_id").val());
       share.save();
     },
 
